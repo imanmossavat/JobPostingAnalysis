5af5f538a107464ae0538e8107c9240a4def1fc5
Iman Mossavat <i874146@fhict.nl>
2024-12-09 14:00:54 +0100

    service manager way of working

---
+++ src/managers/service_manager.py
@@ -0,0 +1,5 @@
+# service manager calls the rest of managers
+# and it is a single point of contact with the system.
+
+# [m1, m2, ... mn] -> service manager <-> system 
+# (system knows nothing about inividual managers) 
